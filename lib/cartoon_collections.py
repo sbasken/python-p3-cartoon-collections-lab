b7f7b1319560c5731262869622e2d02dce548c2f
Saki <sbasken@gmail.com>
2023-03-14 14:55:03 -0700

    complete functions for cartoon collections

--- lib/cartoon_collections.py
+++ lib/cartoon_collections.py
@@ -1,11 +1,31 @@
-def roll_call_dwarves():
-    pass
+def roll_call_dwarves(list):
+    number = 1
+    for name in list:
+        print(f"{number}. {name}")
+        number += 1
 
-def summon_captain_planet():
-    pass
+def summon_captain_planet(calls):
+    new_list = list()
+    for name in calls:
+        new_list.append(name.title() + "!")
+    return new_list
 
-def long_planeteer_calls():
-    pass
+def long_planeteer_calls(calls):
+    longwords = list()
+    for call in calls:
+        if len(call) > 4:
+            longwords.append(call)
+    if len(longwords) > 0:
+        return True
+    else:
+        return False
+        
 
-def find_the_cheese():
+def find_the_cheese(foods):
+    type_of_cheese=["cheddar", "gouda", "camembert"]
+    for food in foods:
+        if food in type_of_cheese:
+            return food
+
+
     pass
